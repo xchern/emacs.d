350f4c70db316a5d6f59f537d97c8dad7e23fd9a
Xiaosong Chen <xiaosong0911@gmail.com>
2015-05-23 14:40:51 +0800

    change css path

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,3 +1,3 @@
 (require-package 'markdown-mode)
-(setq markdown-command "pandoc -s -c C:/styles.css --mathjax --highlight-style espresso")
+(setq markdown-command "pandoc -s -c styles.css --mathjax --highlight-style espresso")
 (provide 'init-markdown)
