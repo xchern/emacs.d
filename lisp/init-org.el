0c611023b4d97198a75ec5176cdab77473dc40af
Xiaosong Chen <xiaosong0911@gmail.com>
2015-05-30 16:14:10 +0800

    set org-mode todo keywords

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,5 +1,11 @@
 (require-package 'org)
 
+(setq org-hide-leading-stars t)
+(setq org-log-done 'time)
+
+(setq org-todo-keywords
+	  '((sequence "TODO(t)" "WAITING(w@/!)" "|" "DONE(d!)"  "CANCELED(c@/@)")))
+
 (org-babel-do-load-languages
    'org-babel-load-languages
    '((emacs-lisp . t)
