e12a598a8c51518885ab78a4416e6d21f23099b3
Xiaosong Chen <xiaosong0911@gmail.com>
2015-06-28 13:26:10 +0800

    Org-mode Key binding

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,5 +1,10 @@
 (require-package 'org)
 
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (setq org-hide-leading-stars t)
 (setq org-log-done 'time)
 
