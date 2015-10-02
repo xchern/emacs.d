9b71dfc43c378dbd4b18d78c4daa40dec353dee9
Xiaosong Chen <xiaosong0911@gmail.com>
2015-10-03 00:11:22 +0800

    Org-mode LaTeX Export For Chinese Modify Org-mode default LaTeX export settings to support Chinese Characters.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -20,3 +20,43 @@
      (sh . t)))
 
 (provide 'init-org)
+
+;; following settings based on https://github.com/wuyao721/51docs/blob/master/it/emacs-org-mode-pdf.org
+;; before using please:
+;;   1.install font STFangsong, SimHei, KaiTi
+;;   2.install texlive packages
+;;   3.inset option: #+LaTeX_CLASS: xecjk-article at beginning
+
+(require 'ox-latex)
+
+(setq org-latex-pdf-process
+	  '("xelatex -interaction nonstopmode -output-directory %o %f"
+		"bibtex %b"
+		"xelatex -interaction nonstopmode -output-directory %o %f"
+		"xelatex -interaction nonstopmode -output-directory %o %f"))
+
+(add-to-list 'org-latex-classes
+		  '("xecjk-article"
+		    "\\documentclass{article}
+[NO-DEFAULT-PACKAGES]
+[PACKAGES]
+\\usepackage[T1]{fontenc}
+\\usepackage{fixltx2e}
+\\usepackage{graphicx}
+\\usepackage{grffile}
+\\usepackage{longtable}
+\\usepackage{wrapfig}
+\\usepackage{rotating}
+\\usepackage{amsmath}
+\\usepackage{textcomp}
+\\usepackage{amssymb}
+\\usepackage{capt-of}
+\\usepackage[colorlinks=true, linkcolor = blue, citecolor = green]{hyperref}
+\\usepackage{xeCJK}
+\\setCJKmainfont[BoldFont=SimHei, ItalicFont=KaiTi]{STFangsong}
+[EXTRA]"
+         ("\\section{%s}" . "\\section*{%s}")
+         ("\\subsection{%s}" . "\\subsection*{%s}")
+         ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+         ("\\paragraph{%s}" . "\\paragraph*{%s}")
+         ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
