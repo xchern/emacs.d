57d9a5976577f90ef2cfa212d19279b140abbdf8
Xiaosong Chen <xiaosong0911@gmail.com>
2016-11-26 02:33:16 +0800

    lua mode added

--- lisp/init-lang.el
+++ lisp/init-lang.el
@@ -5,4 +5,14 @@
             (local-set-key (kbd "C-c C-c") 'haskell-process-load-file))
 		  	(local-set-key (kbd "C-c h") 'hoogle))
 
+;; lua mode
+(require-package 'lua-mode)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+(add-hook 'lua-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-c") 'lua-send-buffer)
+            (setq lua-indent-level 4)))
+          
+
 (provide 'init-lang)
